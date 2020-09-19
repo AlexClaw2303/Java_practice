268fc8099c76559aed93c8e851dd2f0bb666bcb6
Zamoshnikov Alexander <70809274+AlexClaw2303@users.noreply.github.com>
2020-09-19 14:30:45 +0300

    Prac_four_GUI

---
+++ Prac_four/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+public class Main {
+
+    public static void main(String[] args) {
+        new ButtonsTest();
+    }
+}
