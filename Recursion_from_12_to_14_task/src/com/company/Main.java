93731200cad5ea89cbac8129a2dea26d9adbfd5d
Zamoshnikov Alexander <70809274+AlexClaw2303@users.noreply.github.com>
2020-09-22 22:29:34 +0300

    Recursion_digits_task_12

---
+++ Recursion_from_12_to_14_task/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Digits di = new Digits();
+	    di.FillArray();
+        System.out.println("Нечётные числа последовательности:");
+	    di.Recursion();
+	    return;
+    }
+}
