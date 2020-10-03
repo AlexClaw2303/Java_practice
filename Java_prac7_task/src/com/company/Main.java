c46349ac0364df1469d0a169a9d97ebfd98f2723
Zamoshnikov Alexander <70809274+AlexClaw2303@users.noreply.github.com>
2020-10-03 15:11:24 +0300

    Java_prac7_task

--- Java_prac7_task/src/com/company/Main.java
+++ Java_prac7_task/src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] a1 = new int[5];
